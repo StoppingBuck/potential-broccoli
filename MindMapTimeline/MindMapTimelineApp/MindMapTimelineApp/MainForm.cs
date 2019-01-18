72e4ac68a4de0cf7d9d0dc050acf7858b1869cb9
Joen Rommedahl <joen.rommedahl@gmail.com>
2019-01-18 21:03:59 +0100

    Made stuff. Still doesnt run

---
+++ MindMapTimeline/MindMapTimelineApp/MindMapTimelineApp/MainForm.cs
@@ -0,0 +1,14 @@
+using System;
+using Eto.Forms;
+using Eto.Drawing;
+
+namespace MindMapTimelineApp
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
